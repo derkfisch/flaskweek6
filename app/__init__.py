558af7f2d3d0c539fabaa87148323dcb2af0e83c
derkfisch <dlf418@gmail.com>
2023-03-22 16:14:22 -0400

    commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,15 @@
 from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
-#import all of the routes from the routes file into the current package
+#create an instance of sql alchemy to connect our app to the database
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import route 
+
+# import all of the routes from the routes file into the current package
+from app import route, models
--- app/route.py
+++ app/route.py
@@ -1,9 +1,46 @@
 from app import app
+from flask import render_template, redirect, url_for, flash
+from fake_data import posts
+from app.forms import SignUpForm, LoginForm
 
 @app.route('/')
-def hello_world():
-    return "Hello world!"
+def index():
+    return render_template('index.html', posts=posts, logged_in=False)
 
-@app.route('/test')
-def hello_test():
-    return 'This is a test!!!!'
+
+@app.route('/signup', methods=["GET", "POST"])
+def signup():
+    # Create an instance of the form (in the context of the current request)
+    form = SignUpForm()
+    # Check if the form was submitted and that all of the fields are valid
+    if form.validate_on_submit():
+        # If so, get the data from the form fields
+        print('Hooray our form was validated!!')
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+        email = form.email.data
+        username = form.username.data
+        password = form.password.data
+        print(first_name, last_name, email, username, password)
+        flash(f"Thank you {first_name} for signing up!", "success")
+        return redirect(url_for('index'))
+    return render_template('signup.html', form=form)
+
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        print('Form Validated :)')
+        username = form.username.data
+        password = form.password.data
+        print(username, password)
+        # TODO: Check if there is a user with username and that password
+        # Fake an invalid log in
+        if password == 'abc':
+            flash('Invalid username and/or password. Please try again', 'danger')
+            return redirect(url_for('login'))
+
+        flash(f'You have successfully logged in as {username}', 'success')
+        return redirect(url_for('index'))
+    return render_template('login.html', form=form)
